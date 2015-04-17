fe0c581b5ccee9394260140258b346f6f56d88ad
root <root@turnkey.cloud.cybera.ca>
2015-04-17 21:50:26 +0000

    script to upload and run turnkey images

--- turnkey_to_rac.sh
+++ turnkey_to_rac.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-if [ $1 == "" ]; then
+if [ -z "$1" ]; then
+  echo "Specify image name. Example: turnkey.to.RAC openvpn"
   exit
 fi
 
@@ -13,4 +14,4 @@ ramid=$( glance image-create --container-format ari --disk-format ari --name ${1
 kernid=$( glance image-create --container-format aki --disk-format aki --name ${1}_kernel < turnkey-${1}-13.0-wheezy-amd64-kernel | grep id | awk {'print $4'} )
 glance image-create --container-format ami --disk-format ami --name ${1}_img --property kernel_id=${kernid} --property ramdisk_id=${ramid} < turnkey-${1}-13.0-wheezy-amd64.img
 cd ..
-nova boot -flavor m1.micro --image ${1}_img --key-name blogpost --security-groups default my${1}
+nova boot --flavor m1.micro --image ${1}_img --key-name blogpost --security-groups default my${1}
