394f4dac844d1b309ec00b85bfbf826697da9525
root <root@turnkeybootsrap.cloud.cybera.ca>
2015-04-21 19:43:25 +0000

    script to upload TrunLey images to Rapid Access Cloud

--- turnkey_to_rac.sh
+++ turnkey_to_rac.sh
@@ -5,13 +5,16 @@ if [ -z "$1" ]; then
   exit
 fi
 
-source openrc
+echo "Please, enter your Key Pair Name: "
+read blogpost
 
 wget http://mirror.turnkeylinux.org/turnkeylinux/images/openstack/turnkey-${1}-13.0-wheezy-amd64-openstack.tar.gz
 tar -zxvf turnkey-${1}-13.0-wheezy-amd64-openstack.tar.gz
+rm -r turnkey-${1}-13.0-wheezy-amd64-openstack.tar.gz
 cd turnkey-${1}-13.0-wheezy-amd64
 ramid=$( glance image-create --container-format ari --disk-format ari --name ${1}_initrd < turnkey-${1}-13.0-wheezy-amd64-initrd | grep id | awk {'print $4'} )
 kernid=$( glance image-create --container-format aki --disk-format aki --name ${1}_kernel < turnkey-${1}-13.0-wheezy-amd64-kernel | grep id | awk {'print $4'} )
 glance image-create --container-format ami --disk-format ami --name ${1}_img --property kernel_id=${kernid} --property ramdisk_id=${ramid} < turnkey-${1}-13.0-wheezy-amd64.img
 cd ..
-nova boot --flavor m1.micro --image ${1}_img --key-name blogpost --security-groups default my${1}
+rm -r turnkey-${1}-13.0-wheezy-amd64
+nova boot --flavor m1.micro --image ${1}_img --key-name ${blogpost} --security-groups default my${1}
